9d97c0d6fc6c5df910737498bc5cdb91f63e7b30
linkin22 <o.ramogi@gmail.com>
2020-04-14 05:30:34 +0300

    renamed package

---
+++ app/src/main/java/io/okcollect/android/callback/OkHiCallback.java
@@ -0,0 +1,10 @@
+package io.okcollect.android.callback;
+
+import org.json.JSONObject;
+
+/**
+ * Created by ramogiochola on 6/21/16.
+ */
+public abstract class OkHiCallback {
+    public abstract void querycomplete(JSONObject result);
+}
